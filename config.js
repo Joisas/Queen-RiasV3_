69f9e781ee2267e0d3da6b5c1d0da093be31798d
Josias <165520450+Joisas@users.noreply.github.com>
2025-02-06 23:17:26 +0200

    Merci config.js
    Réalisation

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 
 module.exports = {
     prefix: process.env.PREFIX || ".",
-    ownerName: process.env.OWNER_NAME || "Toxxic-Boy",
-    ownerNumber: process.env.OWNER_NUMBER || "2348165846414",
-    mode: process.env.MODE || "public",
+    ownerName: process.env.𝕁𝔸ℙℍ𝔼𝕋_𝔾𝔸𝕀𝕃𝕃𝔸ℝ𝔻 || "Toxxic-Boy",
+    ownerNumber: process.env.0892839065 || "2348165846414",
+    mode: process.env.MODE || "privé",
     region: process.env.REGION || "Nigeria",
-    botName: process.env.BOT_NAME || "Rias Gremory V3",
+    botName: process.env.BOT_NAME || "𝔾𝔸𝕀𝕃𝕃𝔸ℝ𝔻",
     exifPack: process.env.EXIF_PACK || "RIAS V3 LOVES",
     exifAuthor: process.env.EXIF_AUTHOR || "Toxxic",
     timeZone: process.env.TIME_ZONE || "Africa/Lagos",
